9ef20a1cc51e87cde6effdb0279bf0b1590aff99
yuvrajyuvi <yuvrajyuvi0008@gmail.com>
2025-02-18 23:46:53 +0530

    assigned

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -1,5 +1,5 @@
 package com.driver;
-import java.lang.*;
+
 public class Car extends Vehicle {
     private int wheels;
     private String type;
@@ -8,41 +8,82 @@ public class Car extends Vehicle {
     private boolean isManual;
     private int currentGear;
     private int seats;
-    public Car( String name,int wheels, int doors, int gears, boolean isManual,String type, int seats) {
+
+    public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
+        //Hint: Car extends Vehicle
         super(name);
-        this.wheels=wheels;
-        this.doors=doors;
-        this.gears=gears;
-        this.isManual=isManual;
-        this.type =type;
-        this.seats=seats;
-        this.currentGear=1;
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManual = isManual;
+        this.type = type;
+        this.seats = seats;
+        this.currentGear = 1;
     }
 
     public void changeGear(int newGear){
-        this.currentGear=newGear;
+        this.currentGear = newGear;
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
-        super.move(newSpeed,newDirection);
+        super.move(newSpeed, newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
-    public int getWheels(){return wheels;}
-    public String getType(){return type;}
-    public int getDoors(){return doors;}
-    public int getGears(){return gears;}
-    public int getSeats(){return seats;}
-    public int getCurrentgear(){return currentGear;}
-    public boolean isManual(){return isManual ;}
 
-    public void setType(String type){this.type=type;}
-    public void setWheels(int wheels){this.wheels= wheels;}
-    public void setDoors(int doors){this.doors = doors;}
-    public void setGears(int gears){this.gears=gears;}
-    public void setSeats(int seats){this.seats=seats;}
-    public void setManual(boolean m){isManual= m ;}
-    public  void setCurrentGear(int currentGear){
-        this.currentGear =currentGear;
+    public int getWheels() {
+        return wheels;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public int getGears() {
+        return gears;
+    }
+
+    public boolean isManual() {
+        return isManual;
+    }
+
+    public int getCurrentGear() {
+        return currentGear;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setGears(int gears) {
+        this.gears = gears;
+    }
+
+    public void setManual(boolean manual) {
+        isManual = manual;
+    }
+
+    public void setCurrentGear(int currentGear) {
+        this.currentGear = currentGear;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
     }
 }
